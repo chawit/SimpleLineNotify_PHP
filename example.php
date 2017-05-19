5325db061935913a7524149926f90d85c67798f4
Chawit Wiriyakun <chawitmild@gmail.com>
2017-05-19 10:10:25 +0700

    Update example.php

--- example.php
+++ example.php
@@ -7,7 +7,7 @@ $headers = [
 ];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_URL,$pageurl);
+curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'message=hello');
